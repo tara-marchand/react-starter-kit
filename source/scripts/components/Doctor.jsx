4426555619abe91fc21cc8c64e7d06c13807a009
Tara Marchand <tara@mac.com>
2015-12-30 13:20:48 -0800

    Added GSAP (GreenSock Animation Platform)

--- source/scripts/components/Doctor.jsx
+++ source/scripts/components/Doctor.jsx
@@ -2,16 +2,33 @@ import React from 'react';
 import DoctorActions from '../actions/doctors';
 import Button from './Button';
 import Map from './Map';
+import TweenLite from 'gsap';
 
 class Doctor extends React.Component {
     render() {
         return <li className="list-group-item">
             <a href="#"
                 target="_blank"
-                onClick={this.props.handleNameClick}>
+                onClick={this.props.handleNameClick}
+                onMouseOver={this.handleMouseOver}
+                onMouseOut={this.handleMouseOut}>
                     {this.props.doctor.name}
             </a>
         </li>
+    }
+
+    handleMouseOver(e) {
+        var tween = TweenLite.to(e.target, .5, {
+            color: 'red',
+            opacity: .5
+        });
+    }
+
+    handleMouseOut(e) {
+        var tween = TweenLite.to(e.target, .5, {
+            opacity: 1,
+            clearProps: 'color'
+        });
     }
 }
 
